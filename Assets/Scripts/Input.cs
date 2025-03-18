33be7a1e43c5404651b2c1dd711c016aabdfaf2f
rogozilio <rogozin.r998@gmail.com>
2025-03-18 21:51:43 +0700

    Add gameplay mentos gun

--- Assets/Scripts/Input.cs
+++ Assets/Scripts/Input.cs
@@ -12,14 +12,21 @@ namespace Scripts
         public bool PressRightMove => inputActionAsset.FindAction("RightMove").ReadValue<float>() > 0;
         public bool PressForwardMove => inputActionAsset.FindAction("ForwardMove").ReadValue<float>() > 0;
         public bool PressBackMove => inputActionAsset.FindAction("BackMove").ReadValue<float>() > 0;
+        public Vector2 deltaMovePosition => inputActionAsset.FindAction("DeltaMouse").ReadValue<Vector2>();
+
+        public Action OnAction;
 
         private void OnEnable()
         {
             inputActionAsset.Enable();
+
+            inputActionAsset.FindAction("Action").performed += (ctx) => { OnAction(); };
         }
 
         private void OnDisable()
         {
+            inputActionAsset.FindAction("Action").performed -= (ctx) => { OnAction(); };
+            
             inputActionAsset.Disable();
         }
     }
