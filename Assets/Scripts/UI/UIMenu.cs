62fbf52c35e181ece74d7e54bac5cbed43e7e3e1
rogozilio <rogozin.r998@gmail.com>
2025-03-28 16:37:46 +0700

    Add audioMixer, fix computer

--- Assets/Scripts/UI/UIMenu.cs
+++ Assets/Scripts/UI/UIMenu.cs
@@ -1,5 +1,8 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Zenject;
 using Input = Scripts.Input;
@@ -7,13 +10,18 @@ using Input = Scripts.Input;
 public class UIMenu : MonoBehaviour
 {
     [Inject] private Input _input;
-    
+
+    public AudioMixer audioMixer;
     public GameObject mainPanel;
     
     public Button btnContinue;
     public Button btnExit;
     public Slider mouseSensitivity;
-    public Slider volumeSound;
+    public Slider volumeMaster;
+    public Slider volumeMusic;
+    public Slider volumeVoice;
+    public Slider volumeVFX;
+    public Slider volumeCutscene;
 
     private void Awake()
     {
@@ -28,6 +36,27 @@ public class UIMenu : MonoBehaviour
         btnExit.onClick.AddListener(ExitGame);
         
         mouseSensitivity.onValueChanged.AddListener(_input.ChangeMouseSensitivity);
+    }
+
+    private void Start()
+    {
+        ChangeVolume();
+    }
+
+    private void FixedUpdate()
+    {
+        if(!mainPanel.activeSelf) return;
+
+        ChangeVolume();
+    }
+
+    private void ChangeVolume()
+    {
+        audioMixer.SetFloat("Master", math.log10(volumeMaster.value) * 20f);
+        audioMixer.SetFloat("Music", math.log10(volumeMusic.value) * 20f);
+        audioMixer.SetFloat("Voice", math.log10(volumeVoice.value) * 20f);
+        audioMixer.SetFloat("VFX", math.log10(volumeVFX.value) * 20f);
+        audioMixer.SetFloat("Cutscene", math.log10(volumeCutscene.value) * 20f);
     }
 
     private void OnDisable()
